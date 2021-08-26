93e1aba3a585357c27f1b77f4dbcc79cee15734b
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-26 18:39:37 +0530

    started

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Topbar() {
+    return (
+        <div className='topbar'>
+            <div className='topbarWrapper'>
+                <div className='topLeft'>
+                    left
+                </div>
+                <div className='topRight'>
+                    right
+                </div>
+            </div>
+            
+        </div>
+    )
+}
