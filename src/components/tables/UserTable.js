192c277a797d265f2484c641a427a979ca75d906
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-29 01:29:12 +0530

    material ui theming

---
+++ src/components/tables/UserTable.js
@@ -0,0 +1,13 @@
+import { ButtonGroup } from '@material-ui/core'
+import React from 'react'
+import Buttons_textinputs from '../material-ui-components/Buttons_textinputs'
+import Theming from '../material-ui-components/Theming'
+
+export default function UserTable() {
+    return (
+        <div>
+            <Buttons_textinputs/>
+            <Theming/>
+        </div>
+    )
+}
