9f4a18439624bd4cfae2acacfc940816cf813c3a
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-29 01:39:42 +0530

    controls for reusable components

---
+++ src/pages/forms/reusable_comp/Controls.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Input from "./Input";
+import RadioGroup from "./RadioGroup";
+import Select from "./Select";
+import CheckBox from "./CheckBox";
+import DatePicker from "./DatePicker";
+
+const Controls = {
+  Input,
+  RadioGroup,
+  Select,
+  CheckBox,
+  DatePicker,
+};
+
+export default Controls;
