404e522524602176363c68a51b439499f98d366d
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-30 06:23:19 +0530

    bug fixed

--- src/pages/forms/reusable_comp/CheckBox.js
+++ src/pages/forms/reusable_comp/CheckBox.js
@@ -2,14 +2,14 @@ import { FormControlLabel, Checkbox as MuiCheckbox } from "@material-ui/core";
 import React from "react";
 
 export default function CheckBox(props) {
-  const { name, label, value, onchange } = props;
+  const { name, label, value, onChange } = props;
 
   return (
     <FormControlLabel
       control={
         <MuiCheckbox
           checked={value}
-          onChange={onchange}
+          onChange={onChange}
           name={name}
           color="secondary"
         />
