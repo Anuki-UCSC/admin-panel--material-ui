98b5c464bf1802edcb32185c3a61ccc403c04e37
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-30 07:38:21 +0530

    Search

--- src/pages/tables/ReusableTable.js
+++ src/pages/tables/ReusableTable.js
@@ -1,7 +1,22 @@
-import { TableBody, TableCell, TableRow, Toolbar } from "@material-ui/core";
+import {
+  InputAdornment,
+  makeStyles,
+  TableBody,
+  TableCell,
+  TableRow,
+  Toolbar,
+} from "@material-ui/core";
 import React, { useState } from "react";
 import useTable from "./useTable";
 import * as EmployeeService from "../../services/EmployeeService";
+import Controls from "../forms/reusable_comp/Controls";
+import { Filter, Search } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  searchbar: {
+    width: "70%",
+  },
+});
 
 export default function ReusableTable() {
   const headCells = [
@@ -11,14 +26,47 @@ export default function ReusableTable() {
     { id: "deparment", label: "Department" },
   ];
 
+  const classes = useStyles();
   const [records, setRecords] = useState(EmployeeService.getAllEmployees());
+  const [filterFn, setFilterFn] = useState({
+    fn: (items) => {
+      return items;
+    },
+  });
 
   const { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting } =
-    useTable(records, headCells);
+    useTable(records, headCells, filterFn);
+
+  const handleSearch = (e) => {
+    let target = e.target;
+    setFilterFn({
+      fn: (items) => {
+        if (target.value == "") return items;
+        else
+          return items.filter((x) =>
+            x.fullName.toLowerCase().includes(target.value)
+          );
+      },
+    });
+  };
 
   return (
     <>
-      <Toolbar></Toolbar>
+      <Toolbar>
+        <Controls.Input
+          label="Search Employees"
+          className={classes.searchbar}
+          size="small"
+          InputProps={{
+            startAdornemnt: (
+              <InputAdornment position="start">
+                <Search color="primary" />
+              </InputAdornment>
+            ),
+          }}
+          onChange={handleSearch}
+        />
+      </Toolbar>
       <TblContainer>
         <TblHead />
         <TableBody>
