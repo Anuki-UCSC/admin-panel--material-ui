a7b6eae81d5973dda297c5094952c1039c7c9815
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-30 06:23:42 +0530

    bug fixed

--- src/pages/forms/reusable_comp/RadioGroup.js
+++ src/pages/forms/reusable_comp/RadioGroup.js
@@ -8,12 +8,12 @@ import {
 import React from "react";
 
 export default function RadioGroup(props) {
-  const { name, label, value, onchange, radiolist } = props;
+  const { name, label, value, onChange, radiolist } = props;
 
   return (
     <FormControl>
       <FormLabel>{label}</FormLabel>
-      <MuiRadioGroup row name={name} value={value} onChange={onchange}>
+      <MuiRadioGroup row name={name} value={value} onChange={onChange}>
         {radiolist.map((item, index) => (
           <FormControlLabel
             value={item.id}
